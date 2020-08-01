181c2ada74756c47d881f8469d98523a08ed8eb9
Maximum <MAXWILDERSMITH@GMAIL.COM>
2020-08-01 04:08:11 -0700

    Quick fix that should help with directory issues

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import os
 
 path = os.popen('pwd').read()[:-1]+'/'
 with open('run.sh', 'w') as file:
-        file.write('#!/bin/sh\npython3 '+path+'recorder.py 1 python3 '+path+'test.py')
+        file.write('#!/bin/sh\npython3 '+path+'recorder.py 1 "python3 '+path+'test.py"')
 os.system('chmod u+x run.sh')
 
 print("Make sure to open crontab with 'crontab -e' and add the line '@reboot sleep 180 && "+path+"run.sh")
