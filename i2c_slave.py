937f09ce81c2f71b79efc3c1355e7bfcd5dbc495
Maximum <MAXWILDERSMITH@GMAIL.COM>
2020-09-08 22:55:24 -0700

    Updated the pycubed temp i2c code

---
+++ i2c_slave.py
@@ -0,0 +1,25 @@
+import board
+from i2cslave import I2CSlave
+import time
+
+msg = bytes(b'edge detection')
+
+with I2CSlave(board.SCL, board.SDA, [0x5]) as slave:
+	print("starting)")
+	while True:
+		r = slave.request()
+		
+		if not r:
+			print("doing important, blocking work...")
+			time.sleep(2)
+			continue
+		
+		with r:  # Closes the transfer if necessary by sending a NACK or feeding the master dummy bytes
+			if not r.is_read:
+				
+				recieved = r.read(-1)
+				print(recieved)
+				if recieved.decode() == 'Hello there':
+					msg = bytes(b'General Kenobi')
+			elif r.is_read:
+				r.write(msg)
